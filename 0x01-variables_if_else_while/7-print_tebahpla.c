a10e1f1cdd4f3aba1e07855af12c1d218508cd0b
Samrawit Nebiyou <samrawitmerhatsidk@gmail.com>
2022-03-11 20:59:35 +0300

    C

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 122; alpha >= 97; alpha--)
+	{
+		putchar(alpha);
+	}
+	printf("\n");
+
+	return (0);
+}
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Securit
